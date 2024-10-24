ebfd9b9c3b005ce015baac39ee631374079333bd
Achilles06 <92751219+Achilles06@users.noreply.github.com>
2024-10-24 19:11:22 -0400

    commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://toji_ct_end_project_user:Nvn0IQ6MQZ0i3yUTGslRMLbcth9p0DTx@dpg-csdco4jqf0us73b3fkqg-a.oregon-postgres.render.com/toji_ct_end_project','postgresql://33toji:Nvn0IQ6MQZ0i3yUTGslRMLbcth9p0DTx@dpg-csdco4jqf0us73b3fkqg-a:5432/toji_ct_end_project')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     CACHE_TYPE = "SimpleCache"
